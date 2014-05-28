19e0dde4298ba5769249147c4bc982a672a40bb0
ggrandes <guillermo.grandes@gmail.com>
2014-05-28 08:16:57 +0200

    new Mapper: PropertiesMapper

---
+++ src/main/java/org/infra/mapexpression/mapper/PropertiesMapper.java
@@ -0,0 +1,21 @@
+package org.infra.mapexpression.mapper;
+
+import java.util.Properties;
+
+public class PropertiesMapper implements Mapper {
+	private final Properties prop;
+
+	public PropertiesMapper(final Properties prop) {
+		this.prop = prop;
+	}
+
+	@Override
+	public String map(final String input) {
+		if (prop != null) {
+			final String m = prop.getProperty(input);
+			if (m != null)
+				return m;
+		}
+		return null;
+	}
+}
